c0e67876eff3750b7c23301031f5a13d1ad6695f
Belief <749958162@qq.com>
2020-04-17 22:42:43 +0800

    完成了登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+		'space-before-function-paren':0
+    }
+};
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,15 @@
-import Vue from 'vue'
-import { Button } from 'element-ui'
+ import Vue from 'vue'
+// import { Button } from 'element-ui'
+// import { Form, FormItem } from 'element-ui'
+// import { Input } from 'element-ui'
+// // 导入弹框提示组件
+// import { Message } from 'element-ui'
+
+import {Button,Form, FormItem,Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 将Message挂载到vue原型上
+Vue.prototype.$message= Message
